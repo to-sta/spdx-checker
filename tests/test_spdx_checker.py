3f3788499acaf7b72b794db1363b26db752d70a0
tosta <tobias.stach@gmail.com>
2025-06-09 14:49:09 +0200

    changed uv test cmd and updated the tests for root exec

--- tests/test_spdx_checker.py
+++ tests/test_spdx_checker.py
@@ -2,6 +2,10 @@ import spdx_checker
 
 spdx_checker.check_license(
     "MIT",
-    [ "first/example.py", "first/second/example.vue", "first/second/third/example.html", "first/second/third/fourth/example.css"]
+    [
+        "tests/first/example.py",
+        "tests/first/second/example.vue",
+        "tests/first/second/third/example.html",
+        "tests/first/second/third/fourth/example.css",
+    ],
 )
-
