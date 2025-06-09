7b5ef2350232dc112f9b348919951b45159fc438
tosta <tobias.stach@gmail.com>
2025-06-09 15:21:44 +0200

    updated test for the workflow

--- tests/test_spdx_checker.py
+++ tests/test_spdx_checker.py
@@ -1,11 +1,18 @@
 import spdx_checker
+import unittest
 
-spdx_checker.check_license(
-    "MIT",
-    [
-        "tests/first/example.py",
-        "tests/first/second/example.vue",
-        "tests/first/second/third/example.html",
-        "tests/first/second/third/fourth/example.css",
-    ],
-)
+class TestSpdxChecker(unittest.TestCase):
+    def test_check_license_raises_value_error(self):
+        with self.assertRaises(ValueError):
+            spdx_checker.check_license(
+                "MIT",
+                [
+                    "tests/first/example.py",
+                    "tests/first/second/example.vue",
+                    "tests/first/second/third/example.html",
+                    "tests/first/second/third/fourth/example.css",
+                ],
+            )
+
+if __name__ == "__main__":
+    unittest.main()
