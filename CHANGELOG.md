ce208dc37d28a6dc8866a2e6ea4bca89e6a60558
tosta <tobias.stach@gmail.com>
2025-10-25 11:03:12 +0200

    removed setuptools from runtime dependency and added it to the build tools

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,11 @@
 
 All notable changes to this project will be documented in this file.
 
+## [0.1.13]
+
+### Fixed
+
+- Removed setuptools from runtime dependencies and added it to the project's build requirements (build-system).
 
 ## [0.1.12]
 
--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ setup(
     cmdclass={"build_ext": ZigBuilder},
     long_description=(Path(__file__).parent / "README.md").read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
-    py_modules=["builder"],
     packages=["spdx_checker"],
     package_data={"spdx_checker": ["py.typed", "__init__.pyi"]},
     python_requires=">=3.11",
